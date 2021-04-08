b831ebdbda8ad400ce2985618b8b33f6fbfaa18c
Terry Theis <tltheis@prodigy.net>
2021-04-07 19:43:37 -0500

    inquirer add

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'Var heter du?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
+// console.log(profileDataArgs);
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
-console.log(name, github);
+// console.log(name, github);
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
